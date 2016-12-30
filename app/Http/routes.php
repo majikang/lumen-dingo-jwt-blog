5139d5adff9befd8838b6b2019c7893fddce4271
majikang <10307440@qq.com>
2016-12-30 17:35:47 +0800

    vue begin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@
  */
 
 $app->get('/', function () use ($app) {
-    return $app->version();
+    return view('test');
 });
 
 $api = app('Dingo\Api\Routing\Router');
---
+++ resources/views/test.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>vue.js--快速入门Demo</title>
+	<script src="js/vue.min.js"></script>
+	<script src="js/vue-resource.js"></script>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+	<div class="col-md-6 col-md-offset-3">
+		<h1>Vue demo</h1>
+		<div id="app">
+			.......
+		</div>
+	</div>
+</div>
+
+
+<script>
+	new Vue({
+		el:'#app',
+		data:''
+	})
+</script>
+
+</body>
+</html>
